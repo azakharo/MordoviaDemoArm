2db9a434e1eee7197eeceda05b7e41b29442bb77
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2015-05-12 11:05:39 +0300

    dashboard.cards: started the dummy impl and added table with card info.

--- client/app/dashboard/cards.controller.js
+++ client/app/dashboard/cards.controller.js
@@ -1,6 +1,52 @@
 'use strict';
 
-angular.module('demoarmApp')
-  .controller('CardsCtrl', function ($scope) {
-    $scope.cards = ['card 1', 'card 2', 'card 3'];
-  });
+var mod = angular.module('demoarmApp');
+
+mod.controller('CardsCtrl', function ($scope) {
+  $scope.cards = [
+    {
+      id: "80365814",
+      balance: 84,
+      units: 'руб',
+      isActive: true
+    },
+    {
+      id: "80351196",
+      balance: 149,
+      units: 'руб',
+      isActive: true
+    },
+    {
+      id: "80247833",
+      balance: -11,
+      units: 'руб',
+      isActive: false
+    }
+  ];
+
+  var curID = 80247834;
+  var curBalance = 7;
+  for (var i = 0; i < 20; i++) {
+    var newCard = {
+      id: '' + curID,
+      balance: curBalance,
+      units: 'руб',
+      isActive: true
+    };
+    $scope.cards.push(newCard);
+    curID += 1;
+    curBalance += 7;
+  }
+
+});
+
+//mod.filter('cardBalance', function () {
+//  return function (card) {
+//    var balance = card.balance ? card.balance : 0;
+//    var s = "" + balance;
+//    if (card.units) {
+//      s += " " + units;
+//    }
+//    return s;
+//  };
+//});
