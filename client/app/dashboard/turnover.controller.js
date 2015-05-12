315b61806a4e7c07aa2074063824702004c6390a
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2015-05-12 17:10:52 +0300

    turnover: workaround issue with chart width on load.

--- client/app/dashboard/turnover.controller.js
+++ client/app/dashboard/turnover.controller.js
@@ -1,32 +1,40 @@
 'use strict';
 
-angular.module('demoarmApp')
-  .controller('TurnoverCtrl', function ($scope) {
-    $scope.msg = 'Здесь будет график пассажирооборота! Ура!';
+var mod = angular.module('demoarmApp');
 
-    $(function () {
-      $('#container').highcharts({
-        chart: {
-          type: 'bar'
-        },
+mod.controller('TurnoverCtrl', function ($scope, $timeout, $log) {
+
+  function drawChart() {
+    $('#chart').highcharts({
+      chart: {
+        type: 'bar'
+      },
+      title: {
+        text: 'Fruit Consumption'
+      },
+      xAxis: {
+        categories: ['Apples', 'Bananas', 'Oranges']
+      },
+      yAxis: {
         title: {
-          text: 'Fruit Consumption'
-        },
-        xAxis: {
-          categories: ['Apples', 'Bananas', 'Oranges']
-        },
-        yAxis: {
-          title: {
-            text: 'Fruit eaten'
-          }
-        },
-        series: [{
-          name: 'Jane',
-          data: [1, 0, 4]
-        }, {
-          name: 'John',
-          data: [5, 7, 3]
-        }]
-      });
+          text: 'Fruit eaten'
+        }
+      },
+      series: [{
+        name: 'Jane',
+        data: [1, 0, 4]
+      }, {
+        name: 'John',
+        data: [5, 7, 3]
+      }]
     });
-  });
+  }
+
+  drawChart();
+
+  // WARKAROUND issue with chart width
+  $timeout(function() {
+    $('#chart').highcharts().reflow();
+  }, 100);
+
+});
