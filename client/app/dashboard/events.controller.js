41b4853f7b29ed92da295caf9df786d9bee1f636
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2015-05-18 12:22:31 +0300

    New Events section: started, not fully impl-ed yet.

--- client/app/dashboard/events.controller.js
+++ client/app/dashboard/events.controller.js
@@ -2,5 +2,48 @@
 
 angular.module('demoarmApp')
   .controller('EventsCtrl', function ($scope) {
-    $scope.events = ['event 1', 'event 2', 'event 3'];
+
+    var events = [
+      {
+        id: 1,
+        timestamp: moment().subtract(4, 'minutes').toDate(),
+        card: 80365814,
+        operation: 'пополнение',
+        currency: 'баллы',
+        value: 5,
+        isSuccess: true
+      },
+      {
+        id: 2,
+        timestamp: moment().subtract(3, 'minutes').toDate(),
+        card: 80365814,
+        operation: 'списание',
+        currency: 'баллы',
+        value: 5,
+        isSuccess: true
+      },
+      {
+        id: 3,
+        timestamp: moment().subtract(2, 'minutes').toDate(),
+        card: 80365814,
+        operation: 'пополнение',
+        currency: 'баллы',
+        value: 5,
+        isSuccess: true
+      },
+      {
+        id: 4,
+        timestamp: moment().subtract(1, 'minutes').toDate(),
+        card: 80365814,
+        operation: 'списание',
+        currency: 'баллы',
+        value: 5,
+        isSuccess: false
+      }
+    ];
+
+    _(events).reverse();
+
+    $scope.events = events;
+
   });
