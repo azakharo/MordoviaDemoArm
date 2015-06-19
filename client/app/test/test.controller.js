951d32a1183016f7b9a9471f8bf8dbde86d8eeb3
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2015-06-19 14:38:13 +0300

    Impl-ed the test page. You can post turnover values.

--- client/app/test/test.controller.js
+++ client/app/test/test.controller.js
@@ -2,46 +2,24 @@
 
 var mod = angular.module('demoarmApp')
 
-mod.controller('TestCtrl', function ($scope) {
-  $scope.vm = {};
+mod.controller('TestCtrl', function ($scope, $log, myRest) {
+  $scope.userInput = {};
 
-  // The model object that we reference
-  // on the  element in index.html
-  $scope.vm.rental = {};
-
-  // An array of our form fields with configuration
-  // and options set. We make reference to this in
-  // the 'fields' attribute on the  element
-  $scope.vm.rentalFields = [
+  $scope.formFields = [
     {
-      key: 'first_name',
+      key: 'turnover',
       type: 'input',
       templateOptions: {
         type: 'text',
-        label: 'First Name',
-        placeholder: 'Enter your first name',
+        label: 'New turnover value',
+        placeholder: 'Enter integer',
         required: true
       }
-    },
-    {
-      key: 'last_name',
-      type: 'input',
-      templateOptions: {
-        type: 'text',
-        label: 'Last Name',
-        placeholder: 'Enter your last name',
-        required: true
-      }
-    },
-    {
-      key: 'email',
-      type: 'input',
-      templateOptions: {
-        type: 'email',
-        label: 'Email address',
-        placeholder: 'Enter email',
-        required: true
-      }
-    },
+    }
   ];
+
+  $scope.onBtnClick = function() {
+    myRest.postTurnover(moment(), +$scope.userInput.turnover);
+  };
+
 });
