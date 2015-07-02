30a17ed8c851381b1ed38a040426c31271037933
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2015-07-02 13:51:08 +0300

    trans: impl-ed calc successes, failures for specified aggr period. NOT FULLY IMPL-ED YET!

--- client/app/dashboard2/transactions.controller.js
+++ client/app/dashboard2/transactions.controller.js
@@ -3,9 +3,27 @@
 var mod = angular.module('demoarmApp');
 
 mod.controller('TransactionsCtrl', function ($scope, $timeout, $log, myRest) {
+  // Startup code
   $scope.aggrPeriod = 'day';
+  buildChart($scope.aggrPeriod);
 
-  function drawChart() {
+
+  //*****************************************************************
+  // Implementation
+
+  function drawChart(groups) {
+    // Prepare categories
+    var categories = _.map(groups, 'name');;
+
+    // Prepare series data
+    var successSeries = [];
+    var errorSeries = [];
+    groups.forEach(function(grp) {
+      successSeries.push(grp.successes);
+      errorSeries.push(grp.errors);
+    });
+
+    // Draw chart
     $('#trans-chart').highcharts({
       chart: {
         type: 'column',
@@ -18,7 +36,7 @@ mod.controller('TransactionsCtrl', function ($scope, $timeout, $log, myRest) {
         enabled: false
       },
       xAxis: {
-        categories: ['29.06', '30.06', '01.07']
+        categories: categories
       },
       yAxis: {
         min: 0,
@@ -66,17 +84,99 @@ mod.controller('TransactionsCtrl', function ($scope, $timeout, $log, myRest) {
       },
       series: [{
         name: 'Успешных',
-        data: [5, 3, 4],
+        data: successSeries,
         color: '#90EE90'
       }, {
         name: 'Ошибки',
-        data: [2, 2, 3],
+        data: errorSeries,
         color: '#F08080'
       }]
     });
   }
 
-  drawChart();
+  $scope.onAggrPeriodChanged = function() {
+  };
+
+  function buildChart(aggrPeriod) {
+    myRest.getEvents().then(function (events) {
+      // Prepare dummy test data
+      var today = moment();
+      var yesterday = moment().subtract(1, 'days');
+      var beforeYesterday = moment().subtract(2, 'days');
+      var myevents = [];
+      var evt = {};
+
+      // before yesterday
+      evt.timestamp = beforeYesterday;
+      evt.isSuccess = true;
+      myevents.push(evt);
+
+      myevents.push(angular.copy(evt));
+
+      evt = angular.copy(evt);
+      evt.isSuccess = false;
+      myevents.push(evt);
+
+      // yesterday
+      evt = {};
+      evt.timestamp = yesterday;
+      evt.isSuccess = true;
+      myevents.push(evt);
+      myevents.push(angular.copy(evt));
+      myevents.push(angular.copy(evt));
+
+      // today
+      evt = {};
+      evt.timestamp = today;
+      evt.isSuccess = false;
+      myevents.push(evt);
+
+      myevents.push(angular.copy(evt));
+
+      evt = angular.copy(evt);
+      evt.isSuccess = true;
+      myevents.push(evt);
+
+      // Limit data, if necessary
+      ;
+
+      // Aggregate data
+      var groupsObj = _.groupBy(myevents, function(e) {
+        return e.timestamp.format('DD.MM');
+      });
+      //log(groupsObj);
+
+
+      // Calc number of success, failure for every time period
+      var groups = [];
+      Object.keys(groupsObj).forEach(function(grpName) {
+        var group = {};
+        group.name = grpName;
+        //log(grpName);
+        //log(groups[grpName].length);
+        var successes = 0;
+        var errors = 0;
+        groupsObj[grpName].forEach(function(trans) {
+          if (trans.isSuccess) {
+            successes += 1;
+          }
+          else {
+            errors += 1;
+          }
+        });
+        group.successes = successes;
+        group.errors = errors;
+        groups.push(group);
+        //log(grpName + ":");
+        //log("successes: " + successes);
+        //log("errors: " + errors);
+      });
+
+      // Draw chart
+      drawChart(groups);
+    });
+  }
+
 
   function log(msg) {
     $log.debug(msg);
