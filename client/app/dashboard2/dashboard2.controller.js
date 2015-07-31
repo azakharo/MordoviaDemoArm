5ea31b8645a5b2027b218fb5dfffdeacb358f2bc
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2015-07-31 16:28:57 +0300

    optim: use debounce for dashboard redraw on window re-size.

--- client/app/dashboard2/dashboard2.controller.js
+++ client/app/dashboard2/dashboard2.controller.js
@@ -5,8 +5,10 @@ var mod = angular.module('demoarmApp');
 mod.controller('Dashboard2Ctrl', function ($scope, $state, myRest) {
   $scope.baseURL = myRest.getBaseURL();
 
-  $(window).resize(function () {
+  var redrawDashboard = debounce(function() {
     // TODO solve issue with Highcharts and window resizing. The following is work-around.
     $state.reload();
-  });
+  }, 1000);
+
+  $(window).resize(redrawDashboard);
 });
